a26fb8f845969268d6814fd82fe02f381e71ae28
Tito Morales <tito.morales@hpe.com>
2018-12-26 17:43:05 -0600

    Updated message

--- SampleConsoleApp/Program.cs
+++ SampleConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace SampleConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Getting An Update!");
         }
     }
 }
